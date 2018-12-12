06a291ce041c72faa44a56aaa8722b4cabc78cd1
Mai Nguyen Thi Phuong (FTEL ISC HCM) <MaiNTP9@fpt.com.vn>
2018-12-12 15:08:13 +0700

    commit conmeo 3

---
+++ test1/Conmeo3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Conmeo3
+    {
+    #conmeo3
+    x = 1;
+    y = x;
+    x = y;  
+    }
+}
